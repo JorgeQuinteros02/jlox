9a0f7dc49f7df6315b8a802e17737ce68312db93
Jorge Quinteros <jquinteros25@wooster.edu>
2025-05-11 08:41:29 -0400

    added Lox.run

--- src/com/craftinginterpreters/lox/lox.java
+++ src/com/craftinginterpreters/lox/lox.java
@@ -7,8 +7,9 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Scanner;
 
-public class lox {
+public class Lox {
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
             System.out.println("Usage: jlox [script]");
@@ -34,6 +35,16 @@ public class lox {
             String line = reader.readLine();
             if (line == null) break;
             run(line);
+        }
+    }
+
+    private static void run(String source) {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        // For now, just print the tokens.
+        for (Token token : tokens) {
+            System.out.println(token);
         }
     }
 }
